2fa08223750b803bc58a81284a933307096deb86
Andrew Muller <amelnikd@gmail.com>
2015-11-27 17:03:44 +0200

    fffff

--- InsidePoland/WebApp/App_Start/RouteConfig.cs
+++ InsidePoland/WebApp/App_Start/RouteConfig.cs
@@ -12,12 +12,55 @@ namespace WebApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                   name: "ByPolandAspx",
+                   url: "ByPoland.aspx",
+                   defaults: new { controller = "Home", action = "AboutPoland", id = UrlParameter.Optional }
+               );
+            routes.MapRoute(
+                   name: "BNGPolandd",
+                   url: "BNGpoland.aspx",
+                   defaults: new { controller = "Home", action = "VNGPoland", id = UrlParameter.Optional }
+               );
 
+            routes.MapRoute(
+                               name: "Bussiness",
+                               url: "Business.aspx",
+                               defaults: new { controller = "Home", action = "WorkPoland", id = UrlParameter.Optional }
+                           );
+            routes.MapRoute(
+                    name: "cont",
+                    url: "Contacts.aspx",
+                    defaults: new { controller = "Home", action = "Contacts", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
+                    name: "Educat",
+                    url: "Education.aspx",
+                    defaults: new { controller = "Home", action = "EducationPoland", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
+                    name: "pngpoland",
+                    url: "PMGpoland.aspx",
+                    defaults: new { controller = "Home", action = "PMGPoland", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
+                    name: "services",
+                    url: "Services.aspx",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
+                    name: "why",
+                    url: "WhyPoland.aspx",
+                    defaults: new { controller = "Home", action = "AboutPoland", id = UrlParameter.Optional }
+                );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new {controller="home", action = "Index", id = UrlParameter.Optional }
             );
+            
+
+               
         }
     }
 }
